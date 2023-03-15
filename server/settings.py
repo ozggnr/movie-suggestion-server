5d71267f03d5517eff244212f77906786082a290
ozge <ozgecaylioglu@gmail.com>
2023-03-14 20:34:48 -0400

    created .env file to store the secret key

--- server/settings.py
+++ server/settings.py
@@ -11,16 +11,16 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from os import getenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=i%bl)ia-y!ofv@uli$k!w5zop5j!n&lie!o27e9*5pz1a85)1'
+SECRET_KEY = getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
